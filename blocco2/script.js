6b32a8c63c195e6e52eece0181d47756abca8e19
Ottavio42 <ottaviog88@gmail.com>
2021-08-27 12:30:24 +0200

    blocco2

---
+++ blocco2/script.js
@@ -0,0 +1,9 @@
+var numeroU = parseInt(prompt( "inserisci un numero :"));
+
+if ( numeroU % 2 == 0 ) {
+    console.log(numeroU);
+}
+    else {
+        numeroS = (numeroU + 1);
+        console.log(numeroS)
+    }
