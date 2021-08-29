2b1f2834b53beee09b762165c509d56faec36e7c
Ottavio42 <ottaviog88@gmail.com>
2021-08-29 17:03:31 +0200

    fine-blocco-5

---
+++ blocco5/script.js
@@ -0,0 +1,9 @@
+var lungo = [ 1 , 2 , 3 , 4 , 5 , 6 , 7];
+var corto = [1 , 2 , 3, 4];
+
+while (lungo.length > corto.length){
+   corto.push(1)
+}
+
+console.log(lungo.length);
+console.log(corto.length);
