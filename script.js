e1c2bce88874984844bb5dd796ae359a0ceefd65
Ottavio42 <ottaviog88@gmail.com>
2021-08-29 14:57:54 +0200

    metodo-while

--- script.js
+++ script.js
@@ -10,3 +10,18 @@ for ( var i = 0; i < 5 ; i++ ) {
     somma += numero[i];   
 }
 console.log(somma);
+
+// METODO WHILE
+ var somma = 0;
+var i = 1;
+
+while ( i <= 5){
+    var numero = parseInt(prompt( "inserisci un numero :"));
+    while ( isNaN( numero ) ){
+        numero = parseInt(prompt( "inserisci un numero :"));
+    }
+    somma += numero;
+    i++;
+}
+console.log(somma)
+
