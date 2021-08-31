f145ae27035c1f81ebc0b4100f7c6c57815517ac
Ottavio42 <ottaviog88@gmail.com>
2021-08-31 17:07:04 +0200

    fix

--- blocco4/script.js
+++ blocco4/script.js
@@ -1,11 +1,9 @@
-var numeri = [ 10 , 20 , 30 ] ;
+var numeri = [ 10 , 20 , 30 , 40 , 50 ] ;
 somma = 0
 for( i = 0 ; i <=3 ; i++ ){
-    if( i%2 == 0){
-        numeri[i]=0
+    if( i % 2 == 1){
+        somma = somma + numeri[i];
     }
-    else{
-        somma += numeri[i];
-    }
+    
 }
 console.log(somma);
